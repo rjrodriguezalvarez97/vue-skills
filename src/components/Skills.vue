3f29cfe9a000983ad2be35d67f121edfa632c75d
Ricardo Jose Rodriguez Alvarez <rjr15@alu.ua.es>
2018-11-16 18:33:03 +0100

    Add a css

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,51 +1,68 @@
 <template>
-  <div class="hello">
-    <div class="holder">
-      <ul>
-        <li v-for="(data, index) in skills" :key='index'> {{index}}. {{data.skill}} </li>
-      </ul>
-      <!-- <p v-if="skills.length >= 1"> You have more than 1 skills </p>
-      <p v-else> You have less than or equal to 1 skill </p> -->
+    <div class="hello">
+        <div class="holder">
+            <ul>
+            <li v-for="(data, index) in skills" :key='index'> {{data.skill}} </li>
+            </ul>
+            <!-- <p v-if="skills.length >= 1"> You have more than 1 skills </p>
+            <p v-else> You have less than or equal to 1 skill </p> -->
 
-      <div v-bind:class="alertObject">
-      </div>
+        <p> These are the skills that you possess </p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Skills',
-  data() {
-    return {
-      skills: [
-        { "skill": "Vue.js"},
-        { "skill": "Frontend Developer"}
-      ],
-      alertObject: {
-        alert: true,
-        
-      }
+name: 'Skills',
+    data() {
+        return {
+            skills: [
+                { "skill": "Vue.js"},
+                { "skill": "Frontend Developer"}
+            ],
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<!-->
+
+<!--
 // <style  src="./Skills.css" scoped>
 
 // </style>
 -->
 <style scoped>
-  .alert{
-    background-color: yellow;
-    width: 100%;
-    height: 30px;
-  }
-  .another-class{
-    border: 5px solid black;
-  }
+
+    .holder {
+        background: #fff;
+    } 
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+  
+    ul li {
+        padding: 20px;
+        font-size: 1.3em;
+        background-color: #E0EDF4;
+        border-left: 5px solid #3EB3F6;
+        margin-bottom: 2px;
+        color: #3E5252;
+    }
+
+    p {
+        text-align:center;
+        padding: 30px 0;
+        color: gray;
+    }
+
+    .container {
+        box-shadow: 0px 0px 40px lightgray;
+    }
 
 </style>
 
